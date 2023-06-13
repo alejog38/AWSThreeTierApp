8c542d3699018826f602ab9a492bb73145a31376
Alejo Gonzalez <alejog38@gmail.com>
2023-06-13 13:06:57 -0500

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -2,5 +2,5 @@
 yum update -y
 yum -y install curl
 yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 yum install -y nodejs
